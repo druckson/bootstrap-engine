7544af52046cb77bd3221a8d15197fdf3f0bafeb
Dan Erickson <druckson@gmail.com>
2013-12-12 20:54:30 -0800

    add 150k objects

--- src/test.cpp
+++ src/test.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     engine->AddSystem(new Physics());
     engine->AddSystem(new Graphics());
 
-    for (int i=0; i<16000; i++) {
+    for (int i=0; i<150000; i++) {
         Entity e1 = engine->CreateEntity();
     }
 
